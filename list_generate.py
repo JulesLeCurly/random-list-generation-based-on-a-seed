ca6d1917a783f688de2b56a5843c61093e2f13a0
Laurent Depret <jules.depret1@gmail.com>
2023-06-10 18:05:19 +0200

    v5 the best generator i have never see

--- list_generate.py
+++ list_generate.py
@@ -1,42 +1,74 @@
 #V4 module de création de liste
-import time
+import hashlib
+from tqdm import tqdm
 
-class v4(object):
-    def round_number(num):
-        if num - int(num) > 0.5:
-            num += 1
-        return int(num)
+class v5(object):
+	def sha256_decimal(self, text):
+		# Calculer le hachage SHA-256
+		hash_object = hashlib.sha256(text.encode('utf-8'))
+		hex_digest = hash_object.hexdigest()
+
+		# Convertir le résultat hexadécimal en décimal
+		decimal_value = int(hex_digest, 16)
+
+		return decimal_value
     
-    def calculate_large_number(seed, s):
-        current_number = seed
-        turns_count = 1
-        while current_number < 10 ** 1005:
-            turns_count += 1 + s * seed
-            current_number = current_number * turns_count
-        return current_number
+	def calculate_large_number(self, seed):
+		current_number = self.sha256_decimal(str(seed))
+		turns_count = 1
+		while current_number < 10 ** 1030:
+			turns_count += 1
+			chaos = str(self.sha256_decimal(str(current_number+turns_count)))[0]
+			current_number += current_number * turns_count * (int(chaos)+1)
+
+		text = str(current_number)
+		if len(text) > 1000:
+			text = text[:-(len(text)-1000)]
+
+		return int(text)
     
-    def generate(self, seed, Fin, MIN, MAX):
-        seed = int(seed)
-        liste = {}
-        turns_count = 0
-        difference = MAX - MIN
-        difference_in_leng = len(str(difference))
-        difference_in_leng_max = int('9'*difference_in_leng)
-        turns_count_large_number = 0
-        large_number = str(v4.calculate_large_number(seed, difference_in_leng))
-        while Fin != turns_count:
-            turns_count += 1
-            if turns_count_large_number > 1005:
-                large_number = str(v4.calculate_large_number(seed, turns_count))
-                turns_count_large_number = 0
-            
-            t = -1
-            temp = 0
-            while t+1 != difference_in_leng:
-                t += 1
-                turns_count_large_number += 1
-                temp += int(large_number[int(large_number[turns_count_large_number])+int(large_number[turns_count_large_number+1])+int(large_number[turns_count_large_number+2])])*(10**t)
-            
-            temp = (temp*difference)/difference_in_leng_max
-            liste[turns_count] = v4.round_number(temp) + MIN
-        return liste
+	def generate(self, seed, Fin, MIN, MAX, show=False):
+		seed = int(seed)
+		liste = []
+		turns_count = 0
+		difference = MAX - MIN
+		difference_in_leng = len(str(difference))
+		difference_in_leng_max = int('9'*difference_in_leng)
+		turns_count_large_number = 0
+
+		large_number = str(self.calculate_large_number(seed))
+
+		if show:
+			for turns_count in tqdm(range(Fin)):
+				if turns_count_large_number+difference_in_leng > 900:
+					large_number = str(self.calculate_large_number(seed+turns_count))
+					turns_count_large_number = 0
+				
+				t = -1
+				curent_number = ""
+				while t+1 != difference_in_leng:
+					t += 1
+					turns_count_large_number += 1
+					curent_number += large_number[t+turns_count_large_number]
+				
+				curent_number = int(curent_number)
+				curent_number = (curent_number*difference)/difference_in_leng_max
+				liste.append(round(curent_number) + MIN)
+			return liste
+		else:
+			for turns_count in range(Fin):
+				if turns_count_large_number+difference_in_leng > 900:
+					large_number = str(self.calculate_large_number(seed+turns_count))
+					turns_count_large_number = 0
+				
+				t = -1
+				curent_number = ""
+				while t+1 != difference_in_leng:
+					t += 1
+					turns_count_large_number += 1
+					curent_number += large_number[t+turns_count_large_number]
+				
+				curent_number = int(curent_number)
+				curent_number = (curent_number*difference)/difference_in_leng_max
+				liste.append(round(curent_number) + MIN)
+			return liste
